0cf60392bad52a53ef31e4bcf33e8b8b419ed292
Kate <katekarpenko75@gmail.com>
2019-11-08 20:13:55 +0200

    Horse

---
+++ Horse.java
@@ -0,0 +1,22 @@
+package com.nix.module1;
+
+public class Horse {
+    public boolean check(Point point,Point newPoint)
+    { if(point==null ) return false;
+        if(((newPoint.x==point.x+2)&&(newPoint.y==point.y+1)||(newPoint.x==point.x+2)&&(newPoint.y==point.y-1))||((newPoint.x==point.x-2)&&(newPoint.y==point.y+1)||(newPoint.x==point.x-2)&&(newPoint.y==point.y-1))
+                ||((newPoint.y==point.y+2)&&(newPoint.x==point.x+1)||(newPoint.y==point.y+2)&&(newPoint.x==point.x-1))
+                ||((newPoint.y==point.y-2)&&(newPoint.x==point.x+1)||(newPoint.y==point.y-2)&&(newPoint.x==point.x-1)))
+            return true;
+      else  return false;
+    }
+    public static class Point
+    {
+        int x;
+        int y;
+        public Point(int x,int y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+}
